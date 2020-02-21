c95f78d071b7a4d06aa65d796714efe434e15311
xXHachimanXx <globaldep90@gmail.com>
2020-02-21 18:31:27 -0300

    Adicionando cosntantes de controle

--- exercicios/Engenhocas/gira-gira.cpp
+++ exercicios/Engenhocas/gira-gira.cpp
@@ -2,20 +2,27 @@
 #include <iostream>
 #include <wingdi.h>
 
-#define DM_DISPLAYORIENTATION			0x00000080
-#define DMDO_180				0x00000002
+// #define DM_DISPLAYORIENTATION			0x00000080
+// #define DMDO_180				0x00000002
+#define DMDO_DEFAULT    0
+#define DMDO_90         1
+#define DMDO_180        2
+#define DMDO_270        3
 
 using namespace std;
 
-int main() {
+int main() 
+{
     DEVMODE mode;
+
     //first get setting for "current" screen
     EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, & mode);
 
-    if (mode.dmFields | DM_DISPLAYORIENTATION) {
-        mode.dmDisplayOrientation = DMDO_180;
+    if (mode.dmFields | DMDO_DEFAULT) 
+    {
+        mode.dmDisplayOrientation = DMDO_DEFAULT;
         LONG r;
         r = ChangeDisplaySettings( & mode, 0);
-        std::cout << "result: " << r;
+        cout << "result: " << r;
     }
 }
